e41d64ecd1dd6aba3978ba2ce4bc7c1a670886da
gerardrecinto <grecinto@ucsd.edu>
2018-06-25 00:18:25 -0700

    first

---
+++ TritonLinkTests/PageObjects/HomePage.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TritonLinkTests.PageObjects
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='tdr_content_content']/div[1]/div/div/button")]
+        private IWebElement menuElement;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='tdr_content_content']/div[1]/div/div/div/ul/li[2]/a")]
+        private IWebElement myTritonLinkElement;
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://students.ucsd.edu/");
+
+        }
+
+        public void login()
+        {
+            //selfWait(menuElement);
+            //menuElement.Click();
+            selfWait(myTritonLinkElement);
+            myTritonLinkElement.Click();
+        }
+
+        public void selfWait(IWebElement element){
+            wait.Until(ExpectedConditions.ElementToBeClickable(element));
+        }
+
+    }
+}
---
+++ TritonLinkTests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TritonLinkTests.PageObjects;
+namespace TritonLinkTests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            driver = new ChromeDriver(@"/Users/gerardrecinto/Desktop");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Login()
+        {
+            HomePage home = new HomePage(driver);
+            home.goToPage();
+            home.login();
+        }
+
+        [TearDown]
+        public void TearDown(){
+            driver.Close();
+        }
+        static void Main(string[] args)
+        {
+            Tests t = new Tests();
+            t.BeforeEachTest();
+            t.Login();
+            t.TearDown();
+        }
+    }
+}
